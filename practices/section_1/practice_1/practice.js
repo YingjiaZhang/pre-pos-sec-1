e4e17afc8977b16bd080e77ec3e2ddc6628011d6
zhyingjia <965330394@qq.com>
2016-06-06 11:26:48 +0800

    first commit

---
+++ practices/section_1/practice_1/practice.js
@@ -0,0 +1,16 @@
+function collect_same_elements(collection_a, collection_b) {
+    
+  var arr = [];
+  
+  for(var i=0; i<collection_a.length; i++){
+	for(var j=0; j<collection_b.length; j++){
+	   if(collection_a[i] === collection_b[j]){
+			arr.push(collection_a[i]);
+	   }
+   	}
+  }
+
+  return arr;
+}
+
+module.exports = collect_same_elements;
